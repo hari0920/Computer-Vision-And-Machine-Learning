6a8ba8e603a8be113dd46ccc8237d6b40bcb8d52
Hariharan Ramshankar <hariharan.ramshankar@gmail.com>
2017-01-13 15:53:10 -0500

    Added basic5.py

--- OpenCVPython/basic4.py
+++ OpenCVPython/basic4.py
@@ -12,7 +12,16 @@ cv2.rectangle(img, (25, 50), (250, 300), (200, 200, 150), 3, cv2.LINE_AA)
 
 # draw a circle and an ellipse
 cv2.circle(img, (150, 150), 100, (0, 255, 0), 5, cv2.LINE_AA)
-cv2.ellipse(img, (256, 256), (20, 200), 10, 0, 360, (0, 0, 255), 4, cv2.LINE_AA)
+cv2.ellipse(img, (256, 256), (20, 200), 10, 0, 360, (0, 0, 255), -1, cv2.LINE_AA)
+# draw a polygon.
+pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]], np.int32)
+pts = pts.reshape((-1, 1, 2))
+cv2.polylines(img, [pts], True, (0, 255, 255))  # False will notproduce a closed shape
+
+# add text to the image
+font = cv2.FONT_HERSHEY_COMPLEX
+cv2.putText(img, 'OpenCV', (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+
 while (True):
  cv2.imshow("image", img)
  if cv2.waitKey(0) == ord('q'):
