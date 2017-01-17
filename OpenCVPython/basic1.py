c5faf7213c7cbfed09f4a9404bec7aa1fd060eb4
Hariharan Ramshankar <hariharan.ramshankar@gmail.com>
2017-01-16 19:41:28 -0500

    HW0 update

--- OpenCVPython/basic1.py
+++ OpenCVPython/basic1.py
@@ -4,10 +4,10 @@ import cv2
 from matplotlib import pyplot as plt
 # read and display an image using matplotlib and opencv.
 img = cv2.imread("C:\Users\Hariharan\Pictures\suit.jpg")
-im2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # needed to account for the different colour spaces during loading.
-plt.imshow(im2,interpolation='bicubic')
-plt.xticks([]),plt.yticks([])
-plt.show()
+#im2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # needed to account for the different colour spaces during loading.
+#plt.imshow(im2,interpolation='bicubic')
+#plt.xticks([]),plt.yticks([])
+#plt.show()
 # creates a named window that can be resized.
 cv2.namedWindow('suit', cv2.WINDOW_NORMAL)
 cv2.imshow('suit', img)
