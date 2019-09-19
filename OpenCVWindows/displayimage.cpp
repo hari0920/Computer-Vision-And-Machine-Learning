a04597089e90c8d0d660a46d11222381bc8c2a8e
Hariharan Ramshankar <hariharan.ramshankar@gmail.com>
2019-09-18 23:40:14 -0700

    added c++ example

---
+++ OpenCVWindows/displayimage.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+
+int main(void)
+{
+	std::cout << "Simple Program to display an image" << std::endl;
+	//cv::Mat inputImage = cv::imread("D:\\git\\Book-Knowledge\\Book-Knowledge\\Computer-Vision-And-Machine-Learning\\OpenCVWindows\\modifiedtower2.jpg");
+	//cv::imshow("inputImage", inputImage);
+	//cv::waitKey(100);
+	std::getchar();
+	return 0;
+}
