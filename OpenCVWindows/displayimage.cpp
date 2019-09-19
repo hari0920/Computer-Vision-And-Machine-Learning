a6bda139c4229f64d7fb15c37fed11e4ecf0caf7
Hariharan Ramshankar <hariharan.ramshankar@gmail.com>
2019-09-18 23:53:49 -0700

    path fix

--- OpenCVWindows/displayimage.cpp
+++ OpenCVWindows/displayimage.cpp
@@ -4,9 +4,8 @@
 int main(void)
 {
 	std::cout << "Simple Program to display an image" << std::endl;
-	cv::Mat inputImage = cv::imread("..\\..\\..\\modifiedtower2.jpg");
+	cv::Mat inputImage = cv::imread(".\\.\\modifiedtower2.jpg");
 	cv::imshow("inputImage", inputImage);
 	cv::waitKey(100);
-	std::getchar();
 	return 0;
 }
